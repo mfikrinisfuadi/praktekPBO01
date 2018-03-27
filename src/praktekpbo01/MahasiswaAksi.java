bdc75bb46287096d67dc1cf745495d241109069d
mfikrinisfuadi <acengfikry47@gmail.com>
2018-03-27 21:05:26 +0700

    intance object

--- src/praktekpbo01/MahasiswaAksi.java
+++ src/praktekpbo01/MahasiswaAksi.java
@@ -11,7 +11,7 @@ package praktekpbo01;
  */
 public class MahasiswaAksi{ 
         public static void main(String[] args) {
-        
+            Mahasiswa Ronaldinho = new Mahasiswa();
     }
     
 }
